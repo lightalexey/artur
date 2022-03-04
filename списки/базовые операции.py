6fd21e5d82ca01241e5eedc82b69b02048dc10f3
Arturian76 <98603785+Arturian76@users.noreply.github.com>
2022-03-04 19:30:52 +0300

    1

--- списки/базовые операции.py
+++ списки/базовые операции.py
@@ -3,18 +3,35 @@ a = []
 n = 10
 print('Исходный заполненный список:')
 for i in range(n):
-    number = randrange(101) - 50 # случайное число в [-50;50]
+    number = randrange(-5,6)
     a.append(number)
     print(a[i], end=' ')
 print()
 # код начинается отсюда...
 summa = 0
 k2 = 0
+p=1
+sp=0
+kp=0
+x=0
 for i in range(n):
     summa += a[i]
     if a[i] % 2 == 0:
         k2 += 1
-
+    if a[i]!=0:
+        p*=a[i]
+    if a[i]>0:
+        sp+=a[i]
+        kp+=1
+    if a[i]==0:
+       x+=1
 print('Сумма всех элементов массива равна', summa)
 print('2 способ. Сумма всех элементов массива равна', sum(a))
 print('Количество четных элементов:', k2)
+print('Произведение всех элементов:',p)
+sr=summa/n
+print(sr)
+print(sp,kp,sp/kp)
+print(a[0],a[-1])
+print(len(a))
+print('количество нулей:',x)
